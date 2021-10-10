e88256ddfd401079191349b5f9ea536dbf042b15
Rafaela Queiroz <rqueiroz2108@gmail.com>
2021-10-10 14:47:55 -0300

    feat(husky): add husky configuration with prettier

--- src/index.js
+++ src/index.js
@@ -6,33 +6,33 @@
  * @returns {Promise<{ isValid: boolean, areValid: boolean, someValid: boolean }>}
  */
 export const vprops = async (obj, def, property = undefined) => {
-    // Validate a single property
-    const single = (obj, def, prop) => typeof obj[prop] === def[prop]
+  // Validate a single property
+  const single = (obj, def, prop) => typeof obj[prop] === def[prop];
 
-    // Validate all object properties
-    const multiple = (obj, def) => {
-        const keys = Object.keys(def)
-        return keys.every(key => typeof obj[key] === def[key])
-    }
+  // Validate all object properties
+  const multiple = (obj, def) => {
+    const keys = Object.keys(def);
+    return keys.every((key) => typeof obj[key] === def[key]);
+  };
 
-    // Validate if some property is valid
-    const some = (obj, def) => {
-        const keys = Object.keys(def)
-        return keys.some(key => typeof obj[key] === def[key])
-    }
+  // Validate if some property is valid
+  const some = (obj, def) => {
+    const keys = Object.keys(def);
+    return keys.some((key) => typeof obj[key] === def[key]);
+  };
 
-    return new Proxy(obj, {
-        get: (target, prop) => {
-            if (prop === 'isValid') {
-                return single(target, def, property)
-            }
-            if (prop === 'areValid') {
-                return multiple(target, def)
-            }
-            if (prop === 'someValid') {
-                return some(target, def)
-            }
-            return target[prop]
-        }
-    })
-}
+  return new Proxy(obj, {
+    get: (target, prop) => {
+      if (prop === 'isValid') {
+        return single(target, def, property);
+      }
+      if (prop === 'areValid') {
+        return multiple(target, def);
+      }
+      if (prop === 'someValid') {
+        return some(target, def);
+      }
+      return target[prop];
+    },
+  });
+};
